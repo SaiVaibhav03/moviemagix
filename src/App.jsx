8b7da257ddae329517245580eb41737cf46e8867
SaiVaibhav03 <balusavaibhav907@gmail.com>
2024-05-26 18:22:33 +0530

    FirstPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { CssBaseline } from "@mui/material";
 import './App.css'
 import PageRoutes from "./Routes/PageRoutes.jsx"
-
 function App(){
     return(
         <>
