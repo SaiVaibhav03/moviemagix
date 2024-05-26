cd0bd2a1fc1a2c12f7e64507021d881dcdf32c64
SaiVaibhav03 <balusavaibhav907@gmail.com>
2024-05-26 15:41:10 +0530

    package.json/package-lock.json changed

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,12 +1,10 @@
 import { Container } from "@mui/material"
 import Navbar from "../Navbar/Navbar.jsx"
-import Carousel from "./Carousel.jsx"
 
 function HomePage(){
     return(
         <Container maxWidth="xxl" sx={{backgroundColor: "#201f31"}}>
             <Navbar/>
-            <Carousel />
         </Container>
     )
 }
