cc5e198e63496ef943a2b6c173ae567630eef658
SaiVaibhav03 <balusavaibhav907@gmail.com>
2024-05-26 08:00:53 +0530

    deploy check

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './App.jsx'
+import './index.css'
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-});
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App.jsx';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(<App />, document.getElementById('root'));
+// });
 
 
 
